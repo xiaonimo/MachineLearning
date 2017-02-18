b73ee9c19d0390410d90e554e3b8ffceb9d5dc5e
xiaonimo <xiaonimo666@gmail.com>
2017-02-18 10:08:19 +0800

    LR

--- LogisticRegression/func.py
+++ LogisticRegression/func.py
@@ -1,5 +1,7 @@
 import matplotlib.pylab as plt
 import pandas as pd
+from numpy import *
+import time
 
 class LR:
     def load_data(self, file_path):
@@ -8,23 +10,37 @@ class LR:
         train_y = _data.iloc[0:_data.shape[0],2]
         return train_x, train_y
 
-    def print_data(self, x, y):
+    def print_data(self, x, y, weights):
         y1 = y == 1
         d1 = x.loc[y1]
         plt.scatter(d1[[0]], d1[[1]], c='yellow')
         y0 = y == 0
         d0 = x.loc[y0]
         plt.scatter(d0[[0]], d0[[1]], c='red')
+
+        linex = arange(-6, 6, 0.1)
+        liney = (-weights[0] - weights[1]*linex)/weights[2]
+        plt.plot(linex, liney.transpose())
+        plt.title('LR(gd)')
         plt.show()
 
-    def fit(self, x, y, a):
-        k = 1
-        for _x, _y in x, y:
-            k = k - a*_x
+    def _sigmoid(self, x):
+        return 1.0/(1.0+exp(-x))
+
+    def fit(self, x, y, alpha, itr):
+        x = c_[ones(x.shape[0]), x]
+        x = mat(x)
+        y = mat(y).transpose()
+        _weights = ones((x.shape[1], 1))
+        for i in range(itr):
+            h = self._sigmoid(x*_weights)
+            err = y - h
+            _weights = _weights + alpha*x.transpose()*err
+        return _weights
 
 if __name__ == '__main__':
     l = LR()
     x,y = l.load_data('data.csv')
-    print(x.shape, y.shape)
-    l.print_data(x,y)
+    w = l.fit(x, y, 0.01, 600)
+    l.print_data(x, y, w)
 
